4d880e74205cb303570ce44ff86bab8a39136473
Akash Kumar <itsakash314@gmail.com>
2026-06-14 23:07:34 +0530

    exe

---
+++ 900/Chemistry.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+void solve()
+{
+    int n,k; cin >> n >> k;
+    string s; cin >> s;
+    for(int i = 0; i < n; i++) mp[]
+}
+int main() {
+    ll test;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
