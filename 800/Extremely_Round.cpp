14c3b0cf7c96048295c1e8c426f024e326af76cd
Akash Kumar <itsakash314@gmail.com>
2026-06-16 23:17:25 +0530

    Extremely Round

--- 800/Extremely_Round.cpp
+++ 800/Extremely_Round.cpp
@@ -2,16 +2,36 @@
 using namespace std;
 #define ll long long
 #define mod 1000000007
-void solve()
-{
-    
+bool check(int x){
+    int d = 0,z = 0;
+    while(x){
+        if(x % 10 == 0){
+            z++;
+        }
+        d++;
+        x /= 10;
+    }
+    return z == d - 1;
 }
 int main() {
+    vector<int> v;
+    for(int i = 1; i <= 999999; i++){
+        if(check(i)){
+            v.push_back(i);
+        }
+    }
+
     ll test;
     cin>>test;
     while(test--)
     {
-        solve();
+        int n; cin >> n;
+       int ans = 0;
+       for(int i = 0; i < v.size(); i++) {
+        if(v[i] <= n) ans++;
+        else break;
+       }
+       cout<<ans<<endl;
     }
     return 0;
 }
